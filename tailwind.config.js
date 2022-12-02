ca8583feefdf03a799118eb2caea9bcbcfca6b1d
jaasonw <jayywong92@gmail.com>
2022-12-01 18:27:59 -0800

    implement functionality to add x's and o's

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  safelist: [
+    "m-2",
+    "h-28",
+    "w-28",
+    "hover:outline-amber-900",
+    "hover:outline-8",
+  ],
+  // plugins: [require("daisyui")],
 };
